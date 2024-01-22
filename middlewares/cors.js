4e7087d265ccff16d48f18eeea3910fcd16dabf0
Vika Buyavykh <vikabuyavykh@gmail.com>
2024-01-22 20:50:09 +0300

    fix: cors handler

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,4 +1,4 @@
-const allowedCors = ['https://buyavykh-diploma.nomoredomainsmonster.ru', 'http://buyavykh-diploma.nomoredomainsmonster.ru', 'localhost:3000'];
+const allowedCors = ['https://buyavykh-diploma.nomoredomainsmonster.ru', 'http://buyavykh-diploma.nomoredomainsmonster.ru', 'http://localhost:3000'];
 function handleCors(req, res, next) {
   const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
