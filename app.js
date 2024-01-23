fbab56ebe91b03b84e38698e16b315c02409b4ca
Vika Buyavykh <vikabuyavykh@gmail.com>
2024-01-23 10:37:35 +0300

    test cors module

--- app.js
+++ app.js
@@ -6,8 +6,16 @@ const router = require('./routes');
 const handleError = require('./middlewares/handleError');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT, MONGO_URL } = require('./config');
+const cors = require('cors');
 
 const app = express();
+app.use(
+  cors({
+    origin: ['https://buyavykh-diploma.nomoredomainsmonster.ru', 'http://buyavykh-diploma.nomoredomainsmonster.ru', 'http://localhost:3000'],
+    credentials: true,
+    maxAge: 30,
+  })
+);
 
 mongoose.connect(MONGO_URL);
 
